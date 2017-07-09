86df2e785982865992192fa201b9fab86fff522e
George Zinyakov <gz.dev@icloud.com>
2017-07-09 22:44:20 +0300

    Updated layout and release model, added release page

--- web/app.py
+++ web/app.py
@@ -1,12 +1,21 @@
 from flask import render_template
 from index import app, db
-from models import Post
+from models import Release
 
 
 @app.route('/', methods=['GET'])
 def index():
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
-    return render_template('index.html', posts=posts)
+    releases = Release.query.order_by(Release.date_posted.desc()).all()
+    return render_template('index.html', releases=releases)
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/release/<string:release_id>', methods=['GET'])
+def show_release(release_id):
+    release = Release.query.filter(Release.id == release_id).first()
+    return render_template('release.html', release=release)
 
 
 if __name__ == '__main__':
--- web/models.py
+++ web/models.py
@@ -2,14 +2,23 @@ import datetime
 from index import db
 
 
-class Post(db.Model):
+class Release(db.Model):
 
-    __tablename__ = 'posts'
+    __tablename__ = 'releases'
 
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String, nullable=False)
+    id = db.Column(db.String, primary_key=True)
+    artist_id = db.Column(db.String, nullable=False)
+    label_id = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    artist_name = db.Column(db.String, nullable=False)
+    label_name = db.Column(db.String, nullable=False)
+    country = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String, nullable=False)
+    youtube_playlist_id = db.Column(db.String, nullable=False)
+    tags = db.Column(db.String, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, title):
+        self.title = title
         self.date_posted = datetime.datetime.now()
